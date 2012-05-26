9a790cbf4203295fccfbeed2e7d617e34f7fa7b8
Sean Brant <brant.sean@gmail.com>
2012-05-26 17:25:07 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+import codecs
+import os
+from importlib import import_module
+from setuptools import find_packages, setup
+
+import fugleman
+
+
+def read(*paths):
+    with codecs.open(os.path.join(os.path.dirname(__file__), *paths)) as f:
+        return f.read()
+
+
+setup(
+    name='Fugleman',
+    version=fugleman.__version__,
+    description='A simple WSGI app that makes it easy to serve web prototypes.',
+    long_description=read('README.rst'),
+    author='Sean Brant',
+    author_email='brant.sean@gmail.com',
+    license='BSD',
+    packages=find_packages(),
+)
